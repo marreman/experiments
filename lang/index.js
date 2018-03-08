98f38061d5f43101d017ee5b3d1e3ef8e27ce320
Martin Liberg <martin.liberg@izettle.com>
2018-03-08 16:32:43 +0100

    Iterating programming language

--- lang/index.js
+++ lang/index.js
@@ -1,46 +1,32 @@
-const snippet = `define x 1\nprint x x`;
+const fs = require("fs");
+const code = fs.readFileSync("sample-2.easy", "utf-8");
 
-const number = {
-  r: /\d+/,
-  f: (_, n) => n
+const parse = (code) => {
+	let open = 0
+	let close = 0
+	let sub = ''
+
+	Array.from(code.trim()).forEach(char => {
+		sub += char
+
+		switch (char) {
+			case '(': open++; break;
+			case ')': close++; break;
+		}
+
+		console.log(sub, open, close)
+
+		if (!open || !close) {
+			return
+		}
+
+		if (open === close) {
+			console.log("done")
+		}
+	})
+
+	return code
 };
 
-const define = {
-  test: ([x]) => x === "define",
-  // validate => new Error("missing argument")
-  exec: ([_, name, value]) => {
-    return `const ${name} = ${value};`;
-  }
-};
-
-const print = {
-  test: ([x]) => x === "print",
-  // validate => new Error("missing argument")
-  exec: ([_, value]) => `console.log(${value});`
-};
-const rules = [define, print];
-
-const split = del => x => x.split(del);
-const isArray = x => x instanceof Array;
-const apply = rule => x => (rule.test(x) ? rule.exec(x) : x);
-
-const parse = x => split("\n")(x).map(split(" "));
-
-const transform = x => {
-  if (isArray(x) && isArray(x[0])) {
-    return x.map(transform);
-  }
-
-  return rules.map(apply).reduce((res, applyRule) => applyRule(res), x);
-};
-
-const compile = x => {
-  return isArray(x) ? x.join("") : compile(x);
-};
-
-const r = compile(transform(parse(snippet)));
-console.log("SOURCE:");
+const r = parse(code);
 console.log(r);
-console.log("");
-console.log("EVAL:");
-eval(r);
