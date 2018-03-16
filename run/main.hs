81c49c6996bdd2704f76afe3a5f604d0b428b492
Martin Liberg <martin.liberg@izettle.com>
2018-03-16 22:21:17 +0100

    Simplify run script

--- run/main.hs
+++ run/main.hs
@@ -17,51 +17,35 @@ main = do
     currentDirectory <- getCurrentDirectory
     arguments <- getArgs
 
-    putStrLn $ show arguments
-
-    let dockerComposeFile = currentDirectory </> "docker-compose.yml"
-        dockerComposeFileTemp = currentDirectory </> "docker-compose.temp.yml"
+    let dockerComposeFileTemp = currentDirectory </> "docker-compose.temp.yml"
         dockerComposeFileTemplate = currentDirectory </> "docker-compose.yml.template"
-
-    dockerComposeFileExists <- fileExist dockerComposeFile
         
     case arguments of
-        arg:image:subCommand ->
-            if arg == "-i" then do
-                putStrLn $ "Image: " ++ image
-                putStrLn $ "Command: " ++ (show subCommand)
+        image:commandToRunInImage -> do
+            template <- readFile dockerComposeFileTemplate
 
-                template <- readFile dockerComposeFileTemplate
+            let replaceImage = T.replace "IMAGE" $ T.pack image
+                replaceWorkingDir = T.replace "WORKING_DIR" $ T.pack currentDirectory
+                tempFile = (replaceImage . replaceWorkingDir) $ T.pack template
+                commandArgs = ["-f", dockerComposeFileTemp, "run", "app"] ++ commandToRunInImage
 
-                let replaceImage = T.replace "IMAGE" $ T.pack image
-                    replaceWorkingDir = T.replace "WORKING_DIR" $ T.pack currentDirectory
-                    tempFile = (replaceImage . replaceWorkingDir) $ T.pack template
-                    commandArgs = ["-f", dockerComposeFileTemp, "run", "app"] ++ subCommand
+            _ <- writeFile dockerComposeFileTemp $ T.unpack tempFile
+            _ <- callProcess "docker-compose" commandArgs
+            _ <- removeFile dockerComposeFileTemp
 
-                _ <- writeFile dockerComposeFileTemp $ T.unpack tempFile
-                _ <- callProcess "docker-compose" commandArgs
-
-                return ()
-            else
-                putStrLn howToUseInfo
+            return ()
         _ -> 
-            if dockerComposeFileExists then do
-                _ <- createProcess $ shell "docker-compose run app $@"
-                exitSuccess
-            else
-                putStrLn howToUseInfo
+            putStrLn howToUseInfo
 
 howToUseInfo :: String
 howToUseInfo =
     unlines
-        [ ""
-        , "When no local docker-compose.yml is present you need to specify what Docker image to run using the -i flag:"
+        [ "Run a command in any Docker image with the current folder mounted."
         , ""
-        , "  run -i elixir mix new my-app"
-        , "  run -i node:9.8.0 node my-script.js"
+        , "Usage:"
+        , "  run [IMAGE] [COMMAND_TO_RUN_IN_IMAGE]"
         , ""
-        , "Instead of:"
-        , ""
-        , "  run mix new my-app"
-        , "  run node my-script.js"
+        , "Examples:"
+        , "  run elixir mix new my-app"
+        , "  run node:9.8.0 node my-script.js"
         ]
